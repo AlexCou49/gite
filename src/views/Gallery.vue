08b7067ce0dfa4d9881aac524357443d07cc456c
Alexandre Coutant <alexcoutant3@gmail.com>
2020-05-14 14:31:14 +0200

    first commit

---
+++ src/views/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="gallery">
+<h1> Ma galerie </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Gallery'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
